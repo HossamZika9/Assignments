8af596193104efeba8c2eb79ea53e48ed1d9cb09
Ithos <Ithos@HOSSAM>
2024-08-03 16:05:07 +0300

    Add project files.

---
+++ firstAssignments/If Statement.cs
@@ -0,0 +1,48 @@
+﻿// If Satement
+
+
+char sign;
+float num1, num2, result;
+
+
+Console.Write("Enter The Sign : ( + , - , / , * ) : ");
+sign = Convert.ToChar(Console.ReadLine());
+
+Console.Write("Enter First Number: ");
+num1 = Convert.ToSingle(Console.ReadLine());
+Console.Write("Enter Second Number: ");
+num2 = Convert.ToSingle(Console.ReadLine());
+
+if (sign == '+')
+{
+    result = num1 + num2;
+    Console.WriteLine($"{num1} + {num2} = {result}");
+}
+
+else if (sign == '-')
+{
+    result = num1 - num2;
+    Console.WriteLine($"{num1} - {num2} = {result}");
+
+}
+
+else if (sign == '/' || sign == '÷')
+{
+
+    result = num1 / num2;
+    Console.WriteLine($"{num1} / {num2} = {result}");
+
+}
+
+else if (sign == '*')
+{
+
+    result = num1 * num2;
+    Console.WriteLine($"{num1} * {num2} = {result}");
+
+}
+
+else
+{
+    Console.WriteLine("Invalid sign!");
+}
---
+++ firstAssignments/Switch.cs
@@ -0,0 +1,48 @@
+﻿// Switch
+
+
+char sign;
+float num1, num2, result;
+
+
+Console.Write("Enter The Sign : ( + , - , / , * ) : ");
+sign = Convert.ToChar(Console.ReadLine());
+
+Console.Write("Enter First Number: ");
+num1 = Convert.ToSingle(Console.ReadLine());
+Console.Write("Enter Second Number: ");
+num2 = Convert.ToSingle(Console.ReadLine());
+
+
+switch(sign)
+    {
+
+    case '+':
+
+        result = num1 + num2;
+        break;
+
+
+    case '-':
+        result = num1 - num2;
+        break;
+
+
+
+    case '/':
+        result = num1 / num2;
+        break;
+
+
+    case '*':
+        result = num1 * num2;
+        break;
+
+
+    default:
+        Console.WriteLine("Invalid sign!");
+        break;
+
+}
+
+Console.WriteLine($"{num1} {sign} {num2} = {result}");
